0d3c311c96db419d942b7334f334419ab106012f
yogawahyuy <yogawahyuy@gmail.com>
2020-09-14 08:16:47 +0700

    Keluhan Progress

--- app/src/main/java/com/rsip/adminrsimoblie/View/BalasKeluhanActivity.java
+++ app/src/main/java/com/rsip/adminrsimoblie/View/BalasKeluhanActivity.java
@@ -1,16 +1,91 @@
 package com.rsip.adminrsimoblie.View;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
+import android.widget.Toast;
 
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.rengwuxian.materialedittext.MaterialEditText;
 import com.rsip.adminrsimoblie.R;
+import com.rsip.adminrsimoblie.RecyclerView.KeluhanModel;
+import com.rsip.adminrsimoblie.RecyclerView.ListKeluhanActivity;
 
 public class BalasKeluhanActivity extends AppCompatActivity {
+    TextView nama,tanggal,kategori,unit,keluhan,balasan;
+    MaterialEditText edtTextBalas;
+    Button btnBalas;
+    Intent intent;
+    FirebaseUser firebaseUser;
+    DatabaseReference dbrefrence= FirebaseDatabase.getInstance().getReference();
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_balas_keluhan);
+        findView();
+        fillFromIntent();
+        balasKeluhan();
     }
+
+    private void findView(){
+        nama=findViewById(R.id.rec_text_nama);
+        tanggal=findViewById(R.id.rec_text_tgl);
+        kategori=findViewById(R.id.rec_text_kategori);
+        unit=findViewById(R.id.rec_text_unit);
+        keluhan=findViewById(R.id.rec_text_keluhan);
+        //balasan=findViewById(R.id.rec_text_balasan);
+        edtTextBalas=findViewById(R.id.edtTextBalasan);
+        btnBalas=findViewById(R.id.btn_balas);
+        intent=getIntent();
+        firebaseUser= FirebaseAuth.getInstance().getCurrentUser();
+    }
+
+    private void fillFromIntent(){
+        nama.setText(intent.getStringExtra("nama"));
+        tanggal.setText(intent.getStringExtra("tanggal"));
+        kategori.setText(intent.getStringExtra("kategori"));
+        unit.setText(intent.getStringExtra("ruangan"));
+        keluhan.setText(intent.getStringExtra("keluhan"));
+    }
+
+    private void balasKeluhan(){
+        btnBalas.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                KeluhanModel keluhanModel=new KeluhanModel();
+
+                keluhanModel.setIdPembalas(firebaseUser.getUid());
+                keluhanModel.setNama(intent.getStringExtra("nama"));
+                keluhanModel.setTanggal(intent.getStringExtra("tanggal"));
+                keluhanModel.setKategori(intent.getStringExtra("kategori"));
+                keluhanModel.setUnit(intent.getStringExtra("ruangan"));
+                keluhanModel.setKeluhan(intent.getStringExtra("keluhan"));
+                keluhanModel.setSender(intent.getStringExtra("sender"));
+                keluhanModel.setStatusBalas("Dibalas");
+                keluhanModel.setPesanBalasan(edtTextBalas.getText().toString());
+
+                dbrefrence.child("Keluhan").child(intent.getStringExtra("key")).setValue(keluhanModel).addOnCompleteListener(new OnCompleteListener<Void>() {
+                    @Override
+                    public void onComplete(@NonNull Task<Void> task) {
+                        Toast.makeText(BalasKeluhanActivity.this, "Balasan Telah Terkirim", Toast.LENGTH_SHORT).show();
+                        startActivity(new Intent(BalasKeluhanActivity.this, ListKeluhanActivity.class));
+                        finish();
+                    }
+                });
+            }
+        });
+
+    }
+
 }
--- app/src/main/java/com/rsip/adminrsimoblie/View/RincianKeluhanActivity.java
+++ app/src/main/java/com/rsip/adminrsimoblie/View/RincianKeluhanActivity.java
@@ -8,7 +8,10 @@ import android.view.View;
 import android.widget.Button;
 import android.widget.TextView;
 
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
 import com.rsip.adminrsimoblie.R;
+import com.rsip.adminrsimoblie.RecyclerView.ListKeluhanActivity;
 
 public class RincianKeluhanActivity extends AppCompatActivity {
     TextView nama,tanggal,kategori,unit,keluhan,balasan;
@@ -50,7 +53,19 @@ public class RincianKeluhanActivity extends AppCompatActivity {
         btnBalas.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                startActivity(new Intent(RincianKeluhanActivity.this,BalasKeluhanActivity.class));
+                Intent intent1=new Intent(RincianKeluhanActivity.this,BalasKeluhanActivity.class);
+                intent1.putExtra("key",intent.getStringExtra("key"));
+                intent1.putExtra("nama",intent.getStringExtra("nama"));
+                intent1.putExtra("tanggal",intent.getStringExtra("tanggal"));
+                intent1.putExtra("kategori",intent.getStringExtra("kategori"));
+                intent1.putExtra("ruangan",intent.getStringExtra("ruangan"));
+                intent1.putExtra("keluhan",intent.getStringExtra("keluhan"));
+                intent1.putExtra("balasan",intent.getStringExtra("balasan"));
+                intent1.putExtra("sender",intent.getStringExtra("sender"));
+                intent1.putExtra("status",intent.getStringExtra("status"));
+                intent1.putExtra("idpembalas",intent.getStringExtra("idpembalas"));
+                startActivity(intent1);
+                finish();
             }
         });
     }
